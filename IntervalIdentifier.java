e701e1c2d63d4883141168929d329e3eb4b42b45
Nate Browne <natebrowne@outlook.com>
2017-08-04 10:56:57 -0700

    Fix one bug, create another... cleaned up the repetitive code in the project, modified the README, and finally patched everything (I think...)

--- IntervalIdentifier.java
+++ IntervalIdentifier.java
@@ -2,7 +2,7 @@
  * Name: Nate Browne
  * Date: 28 July 2017
  * File: IntervalIdentifier.java
- * Version: 2.0
+ * Version: 2.6
  * This program identifies various types of intervals entered in by the user,
  * calculates them, prints it back, and plays the tones corresponding to it. It
  * plays them starting from middle C and going up.
@@ -27,11 +27,11 @@ public class IntervalIdentifier {
   private static final int OCTAVE = 8;
   private static final int OCTAVE_INTERVAL = 12;
 
-  // User input used by scanner
+  // User input grabbed by the scanner
   private static String qualityChoice;
   private static int noteChoice, intervalChoice;
 
-  // Used to make the note go up the octave
+  // Used to make the note go up the octave if needed
   private static boolean goUp;
 
   // Used when reporting results back to the user
@@ -65,24 +65,24 @@ public class IntervalIdentifier {
       try {
 
         // First, grab the desired starting note
+        System.out.println("Enter the starting note in first.");
         for(int i = 0; i < NOTES.length; i++) {
 
           System.out.println("Note option #" + i + ": " + NOTES[i]);
         }
 
-        System.out.print("Enter the starting note (as a number): ");
+        System.out.print("Please make a selection: ");
 
         // Mod by the length of the array to wrap back around
         noteChoice = scan.nextInt() % NOTES.length;
 
-
         // Next, grab the interval
         System.out.println("Enter in the desired interval to calculate");
         System.out.print("Intervals are entered with the corresponding number. "
-          + "(5th = 5, unison = 1, etc): ");
+          + "(5th = 5, unison = 1 or 0, 3rd = 3, etc): ");
 
-        // Mod by the number of unique notes in a scale to avoid an
-        // ArrayIndexOutOfBoundsException
+        // Mod by the number of unique notes in a scale to keep the circular
+        // nature of scales intact
         originalInterval = scan.nextInt();
         intervalChoice = originalInterval % OCTAVE;
         octaveCounter = originalInterval / OCTAVE;
@@ -94,31 +94,8 @@ public class IntervalIdentifier {
 
         qualityChoice = scan.next();
 
-
-        // Parse which quality user has selected
-        switch(qualityChoice) {
-
-          case "a":
-
-            makeAugmentedInterval(noteChoice, --intervalChoice);
-            break;
-          case "d":
-
-            makeDiminishedInterval(noteChoice, --intervalChoice);
-            break;
-          case "m":
-
-            makeMinorInterval(noteChoice, --intervalChoice);
-            break;
-          case "M":
-
-            makeMajorInterval(noteChoice, --intervalChoice);
-            break;
-          default:
-
-            System.err.println("You entered an invalid quality. Try again.");
-            break;
-        }
+        // Create the interval
+        makeInterval(noteChoice, --intervalChoice, qualityChoice);
 
       // Handle user typing a string for a number
       } catch(InputMismatchException e) {
@@ -148,15 +125,20 @@ public class IntervalIdentifier {
   }
 
   /**
-   * This method calculates and prints out the augmented interval for the
-   * base note entered.
-   * @param note base note to use
-   * @param interval desired interval to create
+   * This one method delegates to the helper methods to create the desired
+   * interval, then changes the quality and prints the results back to the user
+   * along with playing the notes in succession.
+   * @param note base note of interval grabbed from main.
+   * @param interval desired interval to create grabbed from main.
+   * @param quality desired quality of created interval
    */
-  public static void makeAugmentedInterval(int note, int interval) {
+  public static void makeInterval(int note, int interval, String quality) {
 
     // Used to correctly pitch the interval
     int newOctave = 0;
+
+    // Used to correctly output results
+    String created;
 
     // Figure out which interval to create
     switch(interval) {
@@ -195,235 +177,69 @@ public class IntervalIdentifier {
         break;
     }
 
-    // Make the interval augmented
-    newVal = (newVal + HALF_STEP) % NOTES.length;
+    // Figure out which quality to make the interval
+    switch(quality) {
+
+      // Augmented interval
+      case "a":
+
+        newVal = (newVal + HALF_STEP) % NOTES.length;
+
+        created = "augmented";
+        break;
+
+      // Diminished interval
+      case "d":
+
+        newVal = (newVal - HALF_STEP) % NOTES.length;
+
+        // Make sure the correct octave is played and handle the exception
+        if(newVal == -1) {
+
+          newVal = NOTES.length - 1;
+          goUp = false;
+        }
+
+        created = "diminished";
+        break;
+
+      // Minor interval
+      case "m":
+
+        // Eliminate perfect intervals (intervals without minor variants)
+        if(interval != 0 && interval != 3 && interval != 4) {
+
+          newVal = (newVal - HALF_STEP) % NOTES.length;
+
+          // Make sure the correct octave is played and handle the exception
+          if(newVal == -1) {
+
+            newVal = NOTES.length - 1;
+            goUp = false;
+          }
+        }
+
+        created = "minor";
+        break;
+
+      // Major interval
+      case "M":
+
+        created = "major";
+        break;
+
+      // Treat everything else as major as well
+      default:
+
+        created = "major";
+        break;
+    }
 
     // Report result to user
     System.out.println("\nFor the note " + NOTES[note] + ", the interval of an "
-      + "augmented " + originalInterval + " results in the note "
-      + NOTES[newVal] + " " + octaveCounter + " octave(s) above the original " +
+      + created + " " + originalInterval + " results in the note " +
+      NOTES[newVal] + " " + octaveCounter + " octave(s) above the original " +
       "note.");
-
-    // Play the interval back to the user
-    System.out.println("This interval sounds like this: \n");
-    notePlayer.play(NOTES[note] + BASE_NOTE);
-
-    if(goUp) {
-
-      newOctave = BASE_NOTE + 1;
-    } else {
-
-      newOctave = BASE_NOTE + octaveCounter;
-    }
-
-    notePlayer.play(NOTES[newVal] + newOctave);
-  }
-
-  /**
-   * This method calculates and prints out the diminished interval for the
-   * base note entered.
-   * @param note base note to use
-   * @param interval desired interval to create
-   */
-  public static void makeDiminishedInterval(int note, int interval) {
-
-    // Used to correctly pitch the interval
-    int newOctave = 0;
-
-    // Figure out which interval to create
-    switch(interval) {
-
-      case -1:
-
-        newVal = createUnison(note);
-        break;
-      case 0:
-
-        newVal = createUnison(note);
-        break;
-      case 1:
-
-        newVal = createSecond(note);
-        break;
-      case 2:
-
-        newVal = createThird(note);
-        break;
-      case 3:
-
-        newVal = createFourth(note);
-        break;
-      case 4:
-
-        newVal = createFifth(note);
-        break;
-      case 5:
-
-        newVal = createSixth(note);
-        break;
-      case 6:
-
-        newVal = createSeventh(note);
-        break;
-    }
-
-    // Make the interval diminished
-    newVal = (newVal - HALF_STEP) % NOTES.length;
-
-    // Hard code the exception solution in
-    if(newVal == -1) {
-
-      newVal = NOTES.length - 1;
-      goUp = false;
-    }
-
-    // Report result to user
-    System.out.println("\nFor the note " + NOTES[note] + ", the interval of a "
-      + "diminished " + originalInterval + " results in the note "
-      + NOTES[newVal] + " " + octaveCounter + " octave(s) above the original " +
-      "note.");
-
-    // Play the interval back to the user
-    System.out.println("This interval sounds like this: \n");
-    notePlayer.play(NOTES[note] + BASE_NOTE);
-
-    if(goUp) {
-
-      newOctave = BASE_NOTE + 1;
-    } else {
-
-      newOctave = BASE_NOTE + octaveCounter;
-    }
-
-    notePlayer.play(NOTES[newVal] + newOctave);
-  }
-
-  /**
-   * This method calculates and prints out the major interval for the base note
-   * entered.
-   * @param note base note to use
-   * @param interval desired interval to create
-   */
-  public static void makeMajorInterval(int note, int interval) {
-
-    // Used to correctly pitch the interval
-    int newOctave = 0;
-
-    // Figure out which interval to create
-    switch(interval) {
-
-      case -1:
-
-        newVal = createUnison(note);
-        break;
-      case 0:
-
-        newVal = createUnison(note);
-        break;
-      case 1:
-
-        newVal = createSecond(note);
-        break;
-      case 2:
-
-        newVal = createThird(note);
-        break;
-      case 3:
-
-        newVal = createFourth(note);
-        break;
-      case 4:
-
-        newVal = createFifth(note);
-        break;
-      case 5:
-
-        newVal = createSixth(note);
-        break;
-      case 6:
-
-        newVal = createSeventh(note);
-        break;
-    }
-
-    // Report result to user
-    System.out.println("\nFor the note " + NOTES[note] + ", the interval of a "
-      + "major " + originalInterval + " results in the note " + NOTES[newVal]
-      + " " + octaveCounter + " octave(s) above the original note.");
-
-    // Play the interval back to the user
-    System.out.println("This interval sounds like this: \n");
-    notePlayer.play(NOTES[note] + BASE_NOTE);
-
-    if(goUp) {
-
-      newOctave = BASE_NOTE + 1;
-    } else {
-
-      newOctave = BASE_NOTE + octaveCounter;
-    }
-
-    notePlayer.play(NOTES[newVal] + newOctave);
-  }
-
-  /**
-   * This method calculates and prints out the minor interval for the base note
-   * entered.
-   * @param note base note to use
-   * @param interval desired interval to create
-   */
-  public static void makeMinorInterval(int note, int interval) {
-
-    // Used to correctly pitch the interval
-    int newOctave = 0;
-
-    // Figure out which interval to create
-    switch(interval) {
-
-      case -1:
-
-        newVal = createUnison(note);
-        break;
-      case 0:
-
-        newVal = createUnison(note);
-        break;
-      case 1:
-
-        newVal = createSecond(note);
-        break;
-      case 2:
-
-        newVal = createThird(note);
-        break;
-      case 3:
-
-        newVal = createFourth(note);
-        break;
-      case 4:
-
-        newVal = createFifth(note);
-        break;
-      case 5:
-
-        newVal = createSixth(note);
-        break;
-      case 6:
-
-        newVal = createSeventh(note);
-        break;
-    }
-
-    // Eliminate perfect intervals (intervals without minor variants)
-    if(interval != 0 && interval != 3 && interval != 4) {
-
-      newVal = ((newVal - HALF_STEP) + NOTES.length) % NOTES.length;
-    }
-
-    // Report result to user
-    System.out.println("\nFor the note " + NOTES[note] + ", the interval of a "
-      + "minor " + originalInterval + " results in the note " + NOTES[newVal]
-      + " " + octaveCounter + " octave(s) above the original note.");
 
     // Play the interval back to the user
     System.out.println("This interval sounds like this: \n");
